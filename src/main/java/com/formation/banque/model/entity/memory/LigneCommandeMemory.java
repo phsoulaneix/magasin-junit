981ef986951981d79ad2bd8e900f5622e39e7930
SOULANEIX Pierre-henry <pierre-henry.soulaneix@serli.com>
2014-11-17 20:49:09 +0100

    init

--- src/main/java/com/formation/banque/model/entity/memory/LigneCommandeMemory.java
+++ src/main/java/com/formation/banque/model/entity/memory/LigneCommandeMemory.java
@@ -1,8 +1,15 @@
 package com.formation.banque.model.entity.memory;
 
+import com.formation.banque.model.entity.Article;
+
 /**
  * The Class LigneCommandeMemory.
  */
 public class LigneCommandeMemory {
 
+	/** The article. */
+	private Article article;
+
+	/** The quantité. */
+	private Integer quantité;
 }
--- src/main/java/com/formation/banque/model/service/DecouvertException.java
+++
@@ -1,8 +0,0 @@
-package com.formation.banque.model.service;
-
-/**
- * The Class DecouvertException.
- */
-public class DecouvertException extends Exception {
-
-}
